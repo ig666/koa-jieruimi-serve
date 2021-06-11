1841a52b5f337fbcbd6d6a5dd095dc97b7033da6
Qi Chen <qi.chen>
2021-06-11 14:58:18 +0800

    modify

--- src/app/controller/pdf.controller.ts
+++ src/app/controller/pdf.controller.ts
@@ -4,7 +4,7 @@ import * as Joi from 'joi';
 import { CustomError } from '../core/error';
 import { PdfService } from '../service/pdf.service';
 import { Pdf } from '../entities/pdf.entity';
-
+import * as fs from 'fs';
 const schema = Joi.object<any, Pdf>({
   serviceName: Joi.string().min(3).max(30),
   restaurant: Joi.string().max(3).max(30),
@@ -49,4 +49,26 @@ export class PdfController {
     ctx.result = data;
     await next();
   }
+
+  /**
+   * 文件流写入
+   */
+  // @Post()
+  // async saveExcel(ctx: Context, next: () => Promise<any>) {
+  //   if (ctx.request.files) {
+  //     let val = ctx.request.files.file as any;
+  //     // 接收读出流
+  //     const reader = fs.createReadStream(val.path);
+  //     // 创建写入流
+  //     // 3. 指定图片路径文件名（即上传图片存储目录）
+  //     const stream = fs.createWriteStream(val.name);
+  //     // 用管道将读出流 "倒给" 输入流
+  //     reader.pipe(stream);
+  //     // 4.打印上传文件在服器上存储的相对路径
+  //     console.log('uploading %s -> %s', val.name, stream.path);
+  //     // 5.重定向到基于根目录下的静态资源web访问路径，展示图片
+  //     // ctx.redirect(stream.path.substr(6).replace(/\\/g, '/'));
+  //   }
+  //   await next();
+  // }
 }
